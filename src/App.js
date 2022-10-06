4cde438ff19ba90d3553bb31467f0ae795ab5d51
Rakib Hasan <rakibhasanroky0@gmail.com>
2022-10-07 01:01:10 +0600

    img added and darkTheme prodived

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-center text-white bg-green-600 font-bold text-2xl p-8">
-        Welcome to Tailwind React Starter Template
+    <div className="dark">
+      <h1 className="text-center text-white bg-green-600 dark:bg-red-600 font-bold text-2xl p-8">
+        Welcome to Fylo Tailwind React Project
       </h1>
     </div>
   );
